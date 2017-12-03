5137e800324e25518b4c108ebb2d8e4875293205
Alen David Jeđud <alen.jedud@hotmail.com>
2017-12-03 22:24:08 +0100

    Dorađeno: - omogućene migracije baze podataka

---
+++ EOWebShop/Models/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace EOWebShop.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        //public DbSet<Proizvod> Proizvodi { get; set; }
+    }
+}
